5896b088006ff87ed2e1151e6661ab63bdaa632a
mrbennett <matt.bennett91@gmail.com>
2018-03-07 21:22:01 +1300

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sample_build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
